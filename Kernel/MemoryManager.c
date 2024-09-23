4ff110cde183a47004f8dee1d68d5baf7e385a00
Jose Benegas Lynch <jobenegaslynch@itba.edu.ar>
2024-09-23 10:31:44 -0300

    memory manager updated

--- Kernel/MemoryManager.c
+++ Kernel/MemoryManager.c
@@ -1,12 +1,14 @@
 #include "memoryManager.h"
 #include "videoDriver.h"
+#include <stdio.h>
 
 typedef struct memory_manager {
     void * start;
     uint64_t size;
+    uint64_t bitmap_size;
     uint32_t qty_blocks;
     uint32_t used_blocks;
-    uint8_t * bitmap;
+    uint32_t * bitmap;
     uint32_t current;
 } memory_manager;
 
@@ -21,87 +23,141 @@ memory_manager mm;
 
 void allocate_heap() {
     for (uint32_t i = 0; i < mm.qty_blocks; i++) {
-        mm.bitmap[i] =(uint8_t)FREE;
+        mm.bitmap[i] =(uint32_t)FREE;
     }
 }
 
-void mm_init(uint64_t mem_size, void * mem_start) {
+void mm_init(void * mem_start, uint64_t mem_size) {
+
+    uint64_t total_size = mem_size;
+    uint32_t bitmap_size;
 
     if (mem_size > HEAP_SIZE) {
         drawWordColor("NOT ENOUGH MEMORY FOR HEAP INITIALIZATION", WHITE, RED);
         return;
     }
-    mm.start = mem_start;
-    mm.size = mem_size;
+
     mm.qty_blocks = mem_size/BLOCK_SIZE;
+
+    if(mem_size % BLOCK_SIZE != 0) {
+        mm.qty_blocks++;
+        total_size = mm.qty_blocks * BLOCK_SIZE;
+    }
+
+    bitmap_size = mm.qty_blocks/BLOCK_SIZE;
+
+    if(mm.qty_blocks % BLOCK_SIZE != 0) {
+        bitmap_size++;
+    }
+
+    total_size += bitmap_size * BLOCK_SIZE;
+
+    mm.size = total_size;
+    mm.start = mem_start +  bitmap_size * BLOCK_SIZE;
     mm.used_blocks = 0;
-    mm.bitmap = (uint8_t *)mem_start;
-    allocate_heap();
+    mm.bitmap = mem_start;
     mm.current = 0;
+
+    // printf("Heap initialized at %p\n", mm.start);
+
+    allocate_heap();
 }
 
 
-uint8_t find_contiguous_mem(uint8_t req_blocks) {
-    uint8_t cont_blocks = 0;
-    uint8_t start = mm.current;
-    uint8_t index = mm.current;
+void * find_contiguous_mem(uint32_t req_blocks, uint32_t * index) {
+    uint32_t cont_blocks = 0;
+    uint32_t start = mm.current;
+    *index = mm.current;
 
     while (cont_blocks < req_blocks) {
-        if (mm.bitmap[mm.current] == FREE) {
-            cont_blocks++;
-        } else {
+
+        if(mm.current == 0 && cont_blocks > 0) {
             cont_blocks = 0;
-            index = mm.current;
+        }
+
+        if (mm.bitmap[mm.current] == FREE) {
+            if (cont_blocks == 0) {
+                *index = mm.current;
+            }
+            cont_blocks++;
+         } else {
+            cont_blocks = 0;
         }
 
         mm.current = (mm.current + 1) % mm.qty_blocks;
 
         if (mm.current == start) {
-            return -1;
+            return NULL; 
         }
     }
 
-    return index;
-
+    return (void *)(mm.start + (*index) * BLOCK_SIZE);
 }
 
-void allocate_mem(uint8_t index, uint32_t size) {
-    mm.bitmap[index] = (uint8_t)START;
-    for (uint8_t i = 1; i < size; i++) {
-        mm.bitmap[index + i] = (uint8_t)ALLOCATED;
+void allocate_mem(uint32_t index, uint32_t blocks) {
+    mm.bitmap[index] = (uint32_t)START;
+    for (uint32_t i = 1; i < blocks; i++) {
+        mm.bitmap[index + i] = (uint32_t)ALLOCATED;
     }
 }
 
 void * mm_malloc(uint32_t size) {
-    uint8_t required_blocks = (size/BLOCK_SIZE) + 1;
+    uint32_t required_blocks = size/BLOCK_SIZE;
+    uint32_t index = 0;
 
-    int position = find_contiguous_mem(required_blocks);
-
-    if (position == -1) {
+    if (required_blocks > mm.qty_blocks - mm.used_blocks) {
+        // for (int i = 0; i < 10000000; i++) {
+        //     printf("NOT ENOUGH MEMORY FOR ALLOCATION1 required blocks: %d, used blocks: %d, qty blocks: %d\n", required_blocks, mm.used_blocks, mm.qty_blocks);
+        // }
         drawWordColor("NOT ENOUGH MEMORY FOR ALLOCATION", WHITE, RED);
-        return 0;
+        return NULL;
     }
 
-    allocate_mem(position, required_blocks);
+    if (size % BLOCK_SIZE != 0) {
+        required_blocks++;
+    }
 
-    return mm.start + position*BLOCK_SIZE;
+    void *ptr = find_contiguous_mem(required_blocks, &index);
+
+    if (ptr == NULL) {  
+        // for (int i = 0; i < 10000000; i++) {
+        //     printf("NOT ENOUGH MEMORY FOR ALLOCATION2");
+        // }
+        drawWordColor("NOT ENOUGH MEMORY FOR ALLOCATION", WHITE, RED);
+        return NULL;
+    }
+
+    // printf("index : %d\n", index);
+
+    // printf("bitmap: %d\n", mm.bitmap[position]);
+
+    allocate_mem(index, required_blocks);
+
+    // printf("Memory allocated at %p for %d blocks for %d size\n", mm.start + index * BLOCK_SIZE, required_blocks, size);
+
+    mm.used_blocks += required_blocks;
+        
+    return ptr;
 
 }
 
 
 void mm_free(void * ptr) {
-    uint8_t index = (ptr - mm.start) / BLOCK_SIZE;
-    if ((ptr - mm.start) % BLOCK_SIZE != 0 || mm.bitmap[index] != (uint8_t)START) {
-        drawWordColor("INVALID MEMORY ADDRESS",WHITE,RED);
+    uint32_t index = (ptr - mm.start) / BLOCK_SIZE;
+    if (index > mm.qty_blocks || mm.bitmap[index] != (uint32_t)START) {
+
+        // for (int i = 0; i < 10000000; i++) {
+        //     printf("INVALID MEMORY ADDRESS TO FREE");
+        // }
+
+        drawWordColor("INVALID MEMORY ADDRESS TO FREE", WHITE, RED);
         return;
     }
 
     do {
         mm.bitmap[index++] = FREE;
-    }while (mm.bitmap[index] == (uint8_t)ALLOCATED);
+        mm.used_blocks--;
+    }while (index < mm.qty_blocks && mm.bitmap[index] == (uint32_t)ALLOCATED);
 
 }
-
-
-
 
--- Kernel/include/memoryManager.h
+++ Kernel/include/memoryManager.h
@@ -4,14 +4,12 @@
 #include <stdint.h>
 
 #define HEAP_SIZE       0x10000000  // 256MB heap REVISAR
-#define BLOCK_SIZE      64        // 64 bytes per block
-#define NUM_BLOCKS      (HEAP_SIZE / BLOCK_SIZE) // Number of blocks in the heap
-#define BITMAP_SIZE     NUM_BLOCKS // Size of the bitmap
+#define BLOCK_SIZE      32        //  bytes per block
 
 
 void * mm_malloc(uint32_t size);
 void mm_free(void * ptr);
-void mm_init(uint64_t mem_size, void * mem_start);
+void mm_init(void * mem_start, uint64_t mem_size );
 
 
 #endif
